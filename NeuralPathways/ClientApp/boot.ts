76e35aa8dcb866dac4db4b5fdcb117e4e77d019c
Timothy McWatters <timmch2o@gmail.com>
2018-10-17 23:29:22 -0500

    Revert "Adminmainmenuui"

--- NeuralPathways/ClientApp/boot.ts
+++ NeuralPathways/ClientApp/boot.ts
@@ -7,10 +7,7 @@ Vue.use(VueRouter);
 const routes = [
     { path: '/', component: require('./components/home/home.vue.html') },
     { path: '/counter', component: require('./components/counter/counter.vue.html') },
-    { path: '/fetchdata', component: require('./components/fetchdata/fetchdata.vue.html') },
-    { path: '/adminmain', component: require('./components/adminmain/adminmain.vue.html') },
-    { path: '/quizassign', component: require('./components/quizassign/quizassign.vue.html') },
-    { path: '/reports', component: require('./components/reports/reports.vue.html') }
+    { path: '/fetchdata', component: require('./components/fetchdata/fetchdata.vue.html') }
 ];
 
 new Vue({
