3b0251ffe4e23a69f46987bd42b6c383fe484218
TimothyMcWatters <timmch2o@gmail.com>
2018-10-26 20:54:45 -0500

    made Question model

---
+++ NeuralPathways/Models/Question.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NeuralPathways.Models
+{
+    public class Question
+    {
+        public int EquationVariableA { get; set; }
+        public int EquationVariableB { get; set; }
+        public int EquationVariableC { get; set; }
+        public int EquationVariableD { get; set; }
+        public int EquationVariableE { get; set; }
+        public int EquationVariableF { get; set; }
+        public int EquationVariableG { get; set; }
+        public int EquationVariableH { get; set; }
+        public int EquationVariableI { get; set; }
+        public int EquationVariableJ { get; set; }
+        public int EquationVariableK { get; set; }
+        public int EquationVariableL { get; set; }
+        public int EquationVariableM { get; set; }
+
+
+    }
+}
