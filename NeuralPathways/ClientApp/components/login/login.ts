e4fd324e234c14098f9c09e0c10d17cd10539da4
Wen <wx2@students.uwf.edu>
2018-11-24 15:21:25 -0600

    add user validation

--- NeuralPathways/ClientApp/components/login/login.ts
+++ NeuralPathways/ClientApp/components/login/login.ts
@@ -12,45 +12,48 @@ export default class LoginComponent extends Vue {
 
         this.user.email = (<HTMLInputElement>document.getElementById("userEmail")).value;
         this.user.password = (<HTMLInputElement>document.getElementById("userPassword")).value;
-       // this.user.role = $(".role:checked").val();
+    
+        if(this.user.email.length != 0 && this.user.password.length != 0)
+        {
 
-
-
-        $.ajax({
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            url: 'api/User/login',
-            type: 'POST',
-            data: JSON.stringify({
-                email: this.user.email,
-                password: this.user.password,
-               // role: this.user.role
-
-            }),
-            dataType: 'json',
-            success: function (response) {
-
-                //if (response.role = 'student')
-                //{
-                //    window.location.href = "/quiz";
-                //}
-
-                //if (response.role = 'teacher') {
-                //    window.location.href = "/adminmain";
-                //}
-
-                window.location.href = "/quiz";
-                
-
-            },
-            error: function (response) {
-                alert("Login failed :(");
-            }
-
-        });
-
+            $.ajax({
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                url: 'api/User/login',
+                type: 'POST',
+                data: JSON.stringify({
+                    email: this.user.email,
+                    password: this.user.password,
+                   // role: this.user.role
+    
+                }),
+                dataType: 'json',
+                success: function (response) {
+    
+                    //if (response.role = 'student')
+                    //{
+                    //    window.location.href = "/quiz";
+                    //}
+    
+                    //if (response.role = 'teacher') {
+                    //    window.location.href = "/adminmain";
+                    //}
+    
+                    window.location.href = "/quiz";
+                    
+    
+                },
+                error: function (response) {
+                    alert("Login failed :(");
+                }
+    
+            });
+            
+        }else{
+            alert("Please check your username or password");
+        }
 
     }
 
