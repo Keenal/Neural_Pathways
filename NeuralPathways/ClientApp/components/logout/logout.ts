03bf50dd3d28dbc0e54a1689157b06372ebbd9d3
Wes Easton <wpe19d@gmail.com>
2018-12-01 12:55:56 -0600

    cookies complete

---
+++ NeuralPathways/ClientApp/components/logout/logout.ts
@@ -0,0 +1,14 @@
+import Vue from 'vue';
+import { Component } from 'vue-property-decorator';
+import * as Cookies from 'es-cookie';
+
+@Component
+export default class LogOut extends Vue {
+    
+
+    mounted() {
+        console.log("LOGGING OUT");
+        Cookies.remove('role');
+       // window.location.href = "/";
+    }
+}
